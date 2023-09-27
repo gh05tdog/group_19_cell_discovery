4df73d5bc9d0e05f12a7f2691028ad036e5ec31a
olive <oliverbadike@gmail.com>
2023-09-27 15:27:10 +0200

    Optimized some functions

--- src/erode.c
+++ src/erode.c
@@ -1,11 +1,13 @@
+#include <stdlib.h>
 #include "cbmp.h"
 #include "erode.h"
+
 
 #define BLACK 0
 #define WHITE 255
 
-void binary_erode(unsigned char binary[BMP_WIDTH][BMP_HEIGHT], unsigned char eroded[BMP_WIDTH][BMP_HEIGHT],
-                  int *any_pixel_eroded) {
+void binary_erode(unsigned char binary[BMP_WIDTH][BMP_HEIGHT]) {
+    unsigned char temp[BMP_WIDTH][BMP_HEIGHT];
     int structuring_element[5][5] = {
             {0, 1, 1, 1, 0},
             {1, 1, 1, 1, 1},
@@ -13,14 +15,20 @@ void binary_erode(unsigned char binary[BMP_WIDTH][BMP_HEIGHT], unsigned char ero
             {1, 1, 1, 1, 1},
             {0, 1, 1, 1, 0}
     };
-    *any_pixel_eroded = 0;
+
+    // Initialize temp with the same values as binary
+    for (int x = 0; x < BMP_WIDTH; ++x) {
+        for (int y = 0; y < BMP_HEIGHT; ++y) {
+            temp[x][y] = binary[x][y];
+        }
+    }
 
     for (int x = 0; x < BMP_WIDTH; ++x) {
         for (int y = 0; y < BMP_HEIGHT; ++y) {
             unsigned char pixel = binary[x][y];
 
             if (pixel == BLACK) {
-                eroded[x][y] = BLACK;
+                temp[x][y] = BLACK;
             } else {
                 int should_erode = 1;
 
@@ -35,18 +43,21 @@ void binary_erode(unsigned char binary[BMP_WIDTH][BMP_HEIGHT], unsigned char ero
                             neighbor = binary[newX][newY];
                         }
 
-                        if (structuring_element[i + 1][j + 1] == 1 && neighbor == BLACK) {
+                        if (structuring_element[i + 2][j + 2] == 1 && neighbor == BLACK) {
                             should_erode = 0;
                         }
                     }
                 }
 
-                eroded[x][y] = should_erode ? WHITE : BLACK;
-
-                if (should_erode == 0) {
-                    *any_pixel_eroded = 1;
-                }
+                temp[x][y] = should_erode ? WHITE : BLACK;
             }
+        }
+    }
+
+    // Copy the temp image back into binary
+    for (int x = 0; x < BMP_WIDTH; ++x) {
+        for (int y = 0; y < BMP_HEIGHT; ++y) {
+            binary[x][y] = temp[x][y];
         }
     }
 }
