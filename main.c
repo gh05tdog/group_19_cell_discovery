40b75518fac06d8e0353afd52991dcc15ab64bd8
Oliver Badike Hansen <s224754@dtu.dk>
2023-10-01 21:08:11 +0200

    Refactored main file

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
-//To compile (linux/mac): gcc cbmp.c main.c -o main.out -std=c99
-//To run (linux/mac): ./main.out example.bmp example_inv.bmp
-
-//To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
-
 #include <stdlib.h>
 #include <stdio.h>
-#include <intrin.h>
 #include <time.h>
 
 #include "cbmp.h"
@@ -17,7 +10,7 @@
 #include "src/convert_to_binary.h"
 #include "src/cluster_find.h"
 
-Coordinate coordinates[5000]; // Assuming a maximum of 1000 cells
+Coordinate coordinates[5000];
 int coord_index = 0;
 
 //Declaring the array to store the image (unsigned char = unsigned 8 bit)
@@ -34,14 +27,12 @@ int main(int argc, char **argv) {
     clock_t start, end;
     double cpu_time_used;
 
-
     int cells = 0;
 
-    if (argc < 3 || argc > 4) {
-        fprintf(stderr, "Usage: %s <input file path> <output file path> [-PEI]\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <input file path> <output file path>\n", argv[0]);
         exit(1);
     }
-
 
     // Load image from file
     read_bitmap(argv[1], input_image);
@@ -62,22 +53,21 @@ int main(int argc, char **argv) {
     write_bitmap(cluster_image, "../circumference_image.bmp");
 
     start = clock();
-    int i = 0;
     int did_erode = 1;
 
     while (did_erode != 0) {
-        binary_erode(eroded_image,&did_erode);
+        binary_erode(eroded_image, &did_erode);
         cell_check(eroded_image, &cells);
-        i++;
     }
     end = clock();
+
+    for (int i = 0; i < coord_index; i++) {
+        printf("Cell %d: x = %d, y = %d\n", i + 1, coordinates[i].x, coordinates[i].y);
+    }
 
     printf("The numbers of cells found is: %d\n", cells);
 
 
-    for(int i = 0; i < coord_index; i++) {
-        printf("Cell %d: x = %d, y = %d\n", i+1, coordinates[i].x, coordinates[i].y);
-    }
     read_bitmap(argv[1], input_image);
     // Add squares to the original image
     add_squares(input_image, coordinates);
